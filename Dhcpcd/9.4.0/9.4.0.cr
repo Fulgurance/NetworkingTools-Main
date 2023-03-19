fba471670f825f1cb245777ac7473bed751d914b
fulgurance <zohran.londais@gmail.com>
2023-03-19 10:33:54 +0000

    Added Dhcpcd and Wpa-Supplicant installers

---
+++ Dhcpcd/9.4.0/9.4.0.cr
@@ -0,0 +1,40 @@
+class Target < ISM::Software
+
+    def configure
+        super
+
+        configureSource([   "--prefix=/usr",
+                            "--sysconfdir=/etc",
+                            "--libexecdir=/usr/lib/dhcpcd",
+                            "--dbdir=/var/lib/dhcpcd",
+                            "--privsepuser=dhcpcd"],
+                            buildDirectoryPath)
+    end
+
+    def build
+        super
+
+        makeSource([Ism.settings.makeOptions],buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+        makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}var/lib/dhcpcd")
+    end
+
+    def install
+        super
+
+        setPermissions("#{Ism.settings.rootPath}var/lib/dhcpcd",0o700)
+        setOwner("#{Ism.settings.rootPath}var/lib/dhcpcd","dhcpcd","dhcpcd")
+        runGroupAddCommand(["-g","52","dhcpcd"])
+        runUserAddCommand([ "-c","\'dhcpcd PrivSep\'",
+                            "-d","/var/lib/dhcpcd",
+                            "-g","dhcpcd",
+                            "-s","/bin/false",
+                            "-u","52","dhcpcd"])
+    end
+
+end
