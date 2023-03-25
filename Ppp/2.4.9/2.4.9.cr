3ab0a49416f76f73e4b008916a191eb6c0824a19
fulgurance <zohran.londais@gmail.com>
2023-03-25 13:03:11 +0000

    Patched syntax error

--- Ppp/2.4.9/2.4.9.cr
+++ Ppp/2.4.9/2.4.9.cr
@@ -9,7 +9,7 @@ class Target < ISM::Software
     def build
         super
 
-        makeSource(path :buildDirectoryPath)
+        makeSource(path: buildDirectoryPath)
     end
 
     def prepareInstallation
