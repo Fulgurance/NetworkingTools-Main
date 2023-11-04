f9b1c226c1928693d80ebaac3aa33cc066769488
Fulgurance <zohran.londais@gmail.com>
2023-11-04 12:20:53 +0000

    Added networkmanager

---
+++ NetworkManager/1.32.10/1.32.10.cr
@@ -0,0 +1,53 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+
+    def configure
+        super
+
+        runMesonCommand([   "--prefix=/usr",
+                            "--buildtype=release",
+                            "-Dlibaudit=no",
+                            "-Dlibpsl=false",
+                            "-Dnmtui=true",
+                            "-Dovs=false",
+                            "-Dppp=false",
+                            "-Dselinux=false",
+                            "-Dsession_tracking=elogind",
+                            "-Dmodem_manager=false",
+                            "-Dsystemdsystemunitdir=no",
+                            "-Dsystemd_journal=false",
+                            "-Dqt=false",
+                            ".."],
+                            buildDirectoryPath,
+                            {"CXXFLAGS+" => "-O2 -fPIC"})
+    end
+
+    def build
+        super
+
+        runNinjaCommand(path: buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        runNinjaCommand(["install"],buildDirectoryPath,{"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
+
+        moveFile("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/NetworkManager","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/NetworkManager-1.32.10")
+
+        if option("Openrc")
+            prepareOpenrcServiceInstallation("#{workDirectoryPath(false)}/NetworkManager-Init.d","networkmanager")
+        end
+    end
+
+    def install
+        super
+
+        runGroupAddCommand(["-fg","86","netdev"])
+    end
+
+end
