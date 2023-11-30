1605fb80a58c53324831b9d437411162462c18b4
Fulgurance <zohran.londais@gmail.com>
2023-11-30 10:38:48 +0000

    Added new software

---
+++ ModemManager/1.18.12/1.18.12.cr
@@ -0,0 +1,28 @@
+class Target < ISM::Software
+
+    def configure
+        super
+
+        configureSource([   "--prefix=/usr",
+                            "--sysconfdir=/etc",
+                            "--localstatedir=/var",
+                            "--disable-static",
+                            "--disable-maintainer-mode",
+                            "--with-systemd-journal=no",
+                            "--with-systemd-suspend-resume"],
+                            buildDirectoryPath)
+    end
+
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+    end
+
+end
