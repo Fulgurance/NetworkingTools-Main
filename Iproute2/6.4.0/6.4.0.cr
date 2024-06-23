47cf913f8a03f3f05c28f5547917af0669463864
Fulgurance <zohran.londais@gmail.com>
2024-06-23 19:21:00 +0100

    Updated iproute2 installer

--- Iproute2/6.4.0/6.4.0.cr
+++ Iproute2/6.4.0/6.4.0.cr
@@ -1,15 +1,5 @@
 class Target < ISM::Software
     
-    def prepare
-        super
-
-        fileReplaceLineContaining(  path:       "#{buildDirectoryPath}/Makefile",
-                                    text:       "ARPD",
-                                    newText:    "")
-
-        deleteFile("#{buildDirectoryPath}/man/man8/arpd.8")
-    end
-    
     def build
         super
 
