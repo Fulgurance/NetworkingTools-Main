208c59ecd1fc407157fb037aefee9b60cd486c3d
Fulgurance <zohran.londais@gmail.com>
2024-08-23 18:07:32 +0100

    Patched dangerous installation process that can make system unusable

--- Wpa-Supplicant/2.10.0/2.10.0.cr
+++ Wpa-Supplicant/2.10.0/2.10.0.cr
@@ -80,7 +80,9 @@ class Target < ISM::Software
     def install
         super
 
-        runChmodCommand("0755 /usr/sbin")
+        runChmodCommand("0755 /usr/sbin/wpa_cli")
+        runChmodCommand("0755 /usr/sbin/wpa_passphrase")
+        runChmodCommand("0755 /usr/sbin/wpa_supplicant}")
 
         if option("Dbus")
             runChmodCommand("0644 /usr/share/dbus-1/system-services")
